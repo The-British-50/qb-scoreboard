6395fb1a7ed6ffdb1d2d3e48283e8975153ed99d
smitthyy <adamjchapman@pm.me>
2023-03-21 20:52:00 +0000

    Changed scoreboarddata function

--- server.lua
+++ server.lua
@@ -3,6 +3,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 QBCore.Functions.CreateCallback('qb-scoreboard:server:GetScoreboardData', function(_, cb)
     local totalPlayers = 0
     local policeCount = 0
+    local ambulanceCount = 0
     local players = {}
 
     for _, v in pairs(QBCore.Functions.GetQBPlayers()) do
@@ -13,11 +14,15 @@ QBCore.Functions.CreateCallback('qb-scoreboard:server:GetScoreboardData', functi
                 policeCount += 1
             end
 
+            if v.PlayerData.job.name == "ambulance" and v.PlayerData.job.onduty then
+                ambulanceCount += 1
+            end
+
             players[v.PlayerData.source] = {}
             players[v.PlayerData.source].optin = QBCore.Functions.IsOptin(v.PlayerData.source)
         end
     end
-    cb(totalPlayers, policeCount, players)
+    cb(totalPlayers, policeCount, ambulanceCount, players)
 end)
 
 RegisterNetEvent('qb-scoreboard:server:SetActivityBusy', function(activity, bool)
